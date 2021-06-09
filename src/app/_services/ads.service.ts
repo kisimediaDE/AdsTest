3fb5c7b1dbf1e6a62fb473320467132747fc61e3
Simon Kirchner <46489367+vit-skirchner@users.noreply.github.com>
2021-06-09 08:11:19 +0200

    Updated with banner ad test

--- src/app/_services/ads.service.ts
+++ src/app/_services/ads.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AdMobPlus, RewardedAd } from '@admob-plus/capacitor';
+import { AdMobPlus, BannerAd, RewardedAd } from '@admob-plus/capacitor';
 import { Platform } from '@ionic/angular';
 
 @Injectable({
@@ -7,6 +7,9 @@ import { Platform } from '@ionic/angular';
 })
 export class AdsService {
   adInterstitialId: string;
+  adBannerId: string;
+
+  banner: BannerAd;
   rewarded: RewardedAd;
 
   constructor(private platform: Platform) {
@@ -14,9 +17,13 @@ export class AdsService {
     if (this.platform.is('android')) {
       // Example id from https://developers.google.com/admob/android/rewarded
       this.adInterstitialId = 'ca-app-pub-3940256099942544/5224354917';
+      // Example id from https://developers.google.com/admob/android/banner
+      this.adBannerId = 'ca-app-pub-3940256099942544/6300978111';
     } else if (this.platform.is('ios')) {
       // Example id from https://developers.google.com/admob/ios/rewarded
       this.adInterstitialId = 'ca-app-pub-3940256099942544/1712485313';
+      // Example id from https://developers.google.com/admob/ios/banner
+      this.adBannerId = 'ca-app-pub-3940256099942544/2934735716';
     }
   }
 
@@ -25,8 +32,30 @@ export class AdsService {
     await AdMobPlus.start();
     // Load VideoRewardsAd
     await this.loadVideoRewardsAd();
+    // Load BannerAd
+    await this.loadBannerAd();
     // Load Listener for RewardAds
     this.initListener();
+  }
+
+  async loadBannerAd(): Promise<void> {
+    // Create new BannerAd with id
+    this.banner = new BannerAd({
+      adUnitId: this.adBannerId,
+      position: 'bottom'
+    });
+    // Display banner
+    await this.banner.show();
+  }
+
+  async showBannerAd(): Promise<void> {
+    // Show banner
+    await this.banner.show();
+  }
+
+  async hideBannerAd(): Promise<void> {
+    // Hide banner
+    await this.banner.hide();
   }
 
   public async showVideoRewardsAd(): Promise<void> {
